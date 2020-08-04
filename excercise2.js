a5c33d080ea7e77140d085a5385f93f1e5ffdd4b
unknown <reyhanandasetiawan@gmail.com>
2020-08-04 15:20:59 +0700

    dua

---
+++ excercise2.js
@@ -0,0 +1,47 @@
+var tanggal = 4 ;
+var bulan = 6;
+var tahun= 2020 ;
+
+switch(bulan){
+    case 1 :
+        console.log('Januari')
+        break;
+    case 2 :
+        console.log('Februari')
+        break;
+    case 3 :
+        console.log('Maret')
+        break;
+    case 4 :
+        console.log('April')
+        break;
+    case 5 :
+        console.log(`${tanggal} Mei ${tahun}`)
+        break;
+    case 6 :
+        console.log(`${tanggal} Juni ${tahun}`)
+        break;
+    case 7 :
+        console.log('Juli')
+        break;
+    case 8 :
+        console.log('Agustus')
+        break;
+    case 9 :
+        console.log('September')
+        break;
+    case 10 :
+        console.log('Oktober')
+        break;
+    case 11 :
+        console.log('November')
+        break;
+    case 12 :
+        console.log('Desember')
+        break;
+    default:
+        console.log('Bukan Bulan')
+        break;  
+    }
+    
+
